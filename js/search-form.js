b115cf0d6a9ed1d137b6d8b123b0aced07bcd034
Iryna Haiduk <irinahaiduk@ukr.net>
2019-10-15 12:17:39 +0300

    Добавила анимацию

---
+++ js/search-form.js
@@ -0,0 +1,33 @@
+var button = document.querySelector(".search-button");
+var form = document.querySelector(".modal-form");
+var date = form.querySelector("[name=arrival-date]");
+var search = form.querySelector("form");
+var data2 = form.querySelector("[name=depature-date]");
+var adults = form.querySelector("[name=adults]");
+var children = form.querySelector("[name=children]");
+
+"use strict"; (function() { form.classList.add("modal-close");})();
+button.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  form.classList.add("modal-show");
+  date.focus();
+});
+
+search.addEventListener("submit", function (evt) {
+    if (!date.value || !date2.value || !adults.value || !children.value) {
+      evt.preventDefault();
+      form.classList.add("modal-error");
+
+    }
+  });
+
+  window.addEventListener("keydown", function (evt) {
+    if (evt.keyCode === 27) {
+      evt.preventDefault();
+      if (form.classList.contains("modal-show")) {
+        form.classList.remove("modal-show");
+        form.classList.add("modal-error");
+
+      }
+    }
+  });
