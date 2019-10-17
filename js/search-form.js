5361ec128c5868196dbb6e7a790f64379b312090
Iryna Haiduk <irinahaiduk@ukr.net>
2019-10-17 17:15:04 +0300

    Редактируем анимацию в форме

--- js/search-form.js
+++ js/search-form.js
@@ -10,20 +10,24 @@
 	var amount2 = popup.querySelector("[name=children]");
 
   popup.classList.add("modal-close");
-  
-	button.addEventListener("click", function (evt) {
-		evt.preventDefault();
-		popup.classList.add("modal-show");
-	});
-  
-	search.addEventListener("submit", function (evt) {
-		evt.preventDefault();
-		if (!date.value || !date2.value || !amount1.value || !amount2.value) {
-      popup.classList.add("modal-error");
-      
-		}
-	});
 
+  
+  button.addEventListener("click", function (evt) {
+	console.log('нажала на кнопку button и вывела ее значение');
+	console.log(button);
+	evt.preventDefault();
+  
+	console.log('проверяю что возаращает условие popup.classList.contains("modal-show")', popup.classList.contains("modal-show"));
+  
+	if (popup.classList.contains("modal-show")) {
+	  console.log('у popup есть класс modal-show, а значит класс удаляем');
+	  popup.classList.remove("modal-show");
+	} else {
+	  console.log('у popup НЕТ класса modal-show, а значит класс выставляем');
+	  popup.classList.add("modal-show"); 
+	}
+   });
+  	
 	window.addEventListener("keydown", function (evt) {
 		if (evt.keyCode === 27) {
 			evt.preventDefault();
@@ -32,4 +36,17 @@
 			}
 		}
 	});
+	
+	search.addEventListener("submit", function (evt) {
+		evt.preventDefault();
+		if (!date.value || !date2.value || !amount1.value || !amount2.value) {
+			popup.classList.remove("modal-error");
+			
+			popup.classList.add("modal-error");
+		} else { 
+		 search.submit();
+		}
+	   });
+	
+
 })();
