e014f83bde912964c91cba5f6536f7def269ccdb
Iryna Haiduk <irinahaiduk@ukr.net>
2019-10-28 19:13:36 +0200

    Добавила события добавления и убавления количества взрослых и детей при клике на плюс и минус

--- js/search-form.js
+++ js/search-form.js
@@ -6,6 +6,41 @@ var date2 = popup.querySelector("[name=depature-date]");
 var amount1 = popup.querySelector("[name=adults]");
 var amount2 = popup.querySelector("[name=children]");
 
+var minus_adults = popup.querySelector("#minus-adults");
+var plus_adults = popup.querySelector("#plus-adults");
+var amount_adults = popup.querySelector("#adults-amount");
+
+var minus_children = popup.querySelector("#minus-children");
+var plus_children = popup.querySelector("#plus-children");
+var amount_children = popup.querySelector("#children-amount");
+
+minus_adults.addEventListener("click", function (evt) {
+	evt.preventDefault();
+	if (amount_adults.value==0) return;
+	amount_adults.value=parseInt(amount_adults.value)-1;
+	}
+);
+
+plus_adults.addEventListener("click", function (evt) {
+	evt.preventDefault();
+	amount_adults.value=parseInt(amount_adults.value)+1;
+	}
+);
+
+plus_children.addEventListener("click", function (evt) {
+	evt.preventDefault();
+	amount_children.value=parseInt(amount_children.value)+1;
+	}
+);
+
+minus_children.addEventListener("click", function (evt) {
+	evt.preventDefault();
+	if (amount_children.value==0) return;
+	amount_children.value=parseInt(amount_children.value)-1;
+	}
+);
+
+
 popup.classList.add("modal-close");
 
 
